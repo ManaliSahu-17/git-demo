51a048c0793e980ed467a1b598da99607e37cf62
ManaliSahu-17 <manali.sahu_cs20@gla.ac.in>
2025-01-31 12:21:17 +0530

    Manali's first commit

---
+++ src/main/java/com/git_demo/git_demo/App1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.git_demo.git_demo;
+
+/**
+ * @author HP
+ *
+ */
+public class App1 {
+	public void add(String data) {
+		System.out.println("dummy method");
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!!");
+		App1 a1 = new App1();
+        a1.add("Data");
+
+	}
+
+}
